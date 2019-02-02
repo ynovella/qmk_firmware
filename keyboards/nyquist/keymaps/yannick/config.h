68a832b466e5aac2162a6c488d63e1cd4eefc270
Yannick <yannick.novella@dougs.fr>
2019-02-02 14:16:52 +0100

    my custom layout

--- keyboards/nyquist/keymaps/yannick/config.h
+++ keyboards/nyquist/keymaps/yannick/config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #undef RGBLED_NUM
@@ -34,3 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#define COMBO_COUNT 2
+#define COMBO_TERM 200
